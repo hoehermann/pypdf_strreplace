4c83a576422352f9741ee147288789f4ecd59f82
Hermann Höhne <hoehermann@gmx.de>
2024-02-22 12:21:52 +0100

    Try to extract text so it can be selected for the search.

--- pypdf_strreplace.py
+++ pypdf_strreplace.py
@@ -1,85 +1,93 @@
 #!/usr/bin/env python3
 import argparse
+import sys
+import io
 import pypdf
 from typing import Any, Callable, Dict, Tuple, Union, cast
-from pypdf.generic import DictionaryObject, NameObject, RectangleObject, EncodedStreamObject, DecodedStreamObject
+from pypdf.generic import DictionaryObject, NameObject, RectangleObject, ContentStream
 from pypdf.constants import PageAttributes as PG
 from pypdf._cmap import build_char_map
 
-# from https://github.com/py-pdf/pypdf/blob/27d0e99/pypdf/_page.py#L1546
-def get_char_maps(obj: Any, space_width: float = 200.0):
-    cmaps: Dict[
-        str,
-        Tuple[
-            str, float, Union[str, Dict[int, str]], Dict[str, str], DictionaryObject
-        ],
-    ] = {}
-    objr = obj
-    while NameObject(PG.RESOURCES) not in objr:
-        # /Resources can be inherited sometimes so we look to parents
-        objr = objr["/Parent"].get_object()
-    resources_dict = cast(DictionaryObject, objr[PG.RESOURCES])
-    if "/Font" in resources_dict:
-        for f in cast(DictionaryObject, resources_dict["/Font"]):
-            cmaps[f] = build_char_map(f, space_width, obj)
-    return {cmap[4]["/BaseFont"]:cmap[3] for cmap in cmaps.values()}
+# TODO: does something like this exist somewhere in PyPDF already?
+# TODO: find out if ascii is an appropriate output encoding
+def operations_to_bytes(operations):
+    data = b""
+    def operand_to_bytes(operand, operation):
+        #print(f"{operand} is of type {type(operand)}")
+        if (isinstance(operand, pypdf.generic._data_structures.ArrayObject)):
+            return b"["+b"".join([operand_to_bytes(o, operation) for o in operand])+b"]"
+        elif (isinstance(operand, pypdf.generic._base.ByteStringObject)):
+            return b"("+operand.original_bytes.replace(b"(",br"\(").replace(b")",br"\)")+b")"
+        elif (isinstance(operand, pypdf.generic._base.FloatObject)):
+            return f"{operand:.2f}".encode("ascii")
+        else:
+            return str(operand).encode("ascii")
+    for operands, operation in operations:
+        if (operands):
+            operands = [operand_to_bytes(o, operation) for o in operands]
+            data += b" ".join(operands)+b" "
+        data += operation+b"\n"
+    return data
+
+def squash_TJs(operations):
+    for operands, operation in operations:
+        if (operation == b"TJ"):
+            operands[0] = pypdf.generic._base.ByteStringObject(b''.join([o.original_bytes for o in operands[0] if isinstance(o, pypdf.generic._base.ByteStringObject)]))
+            operation = b"Tj"
+        yield (operands, operation)
+
+def squash_TdTjs(operations, collapsible_width):
+    def _squash_TdTjs(operations, collapsible_width):
+        vertical_offset = 0
+        horizontal_offset = 0
+        previous_Tj_operands = None
+        for operands, operation in operations:
+            if (operation == b"Td"):
+                horizontal_offset = int(operands[0])
+                vertical_offset = int(operands[1])
+            elif (operation == b"Tj"):
+                if (vertical_offset == 0 and previous_Tj_operands and  horizontal_offset >= 0 and horizontal_offset < collapsible_width):
+                    # found a Tj with no vertical adjustment
+                    # → character is on same line
+                    # → append character to previous Tj, ignore this Tj
+                    previous_Tj_operands[0] = pypdf.generic._base.ByteStringObject(previous_Tj_operands[0] + operands[0])
+                    continue
+                else:
+                    previous_Tj_operands = operands
+            yield (operands, operation)
+    # _squash_TdTjs modifies operators by reference retroactively.
+    # _squash_TdTjs must iterate over the entire list to be effective.
+    return list(_squash_TdTjs(operations, collapsible_width))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Replace text in a PDF file.')
     parser.add_argument('--input')
-    parser.add_argument('--output')
+    parser.add_argument('--output', required=False)
+    parser.add_argument('--collapsible_width', default=25)
     parser.add_argument('--papersize', default="A4")
-    parser.add_argument('--font', required=False)
-    parser.add_argument('--search', required=False)
-    parser.add_argument('--replace', required=False)
     args = parser.parse_args()
-    total_replacements = 0
     reader = pypdf.PdfReader(args.input)
     writer = pypdf.PdfWriter()
-
+    
     for page_index, page in enumerate(reader.pages):
         print(f"Processing page {page_index+1}…")
-
-        cmaps = get_char_maps(page)
-        for fontname in cmaps.keys():
-            if (not args.font):
-                print(fontname)
-            elif (fontname.endswith(args.font)):
-                args.font = fontname
-        if (not args.font):
-            continue
-
-        charmap = cmaps[args.font]
-        reverse_charmap = {v:k for k,v in charmap.items()}
-        def full_to_subsetted(full):
-            subsetted = ''.join([reverse_charmap[c] for c in full])
-            subsetted = subsetted.replace(r'(',r'\(').replace(r')',r'\)') # TODO: which other characters must be escaped?
-            return subsetted.encode('ascii') # TODO: use original cmap[2] here
-        search = full_to_subsetted(args.search)
-        replace = full_to_subsetted(args.replace)
-
-        page_replacements = 0
-        # based on https://stackoverflow.com/questions/41769120/search-and-replace-for-text-within-a-pdf-in-python#69276885
+        #page.extract_text()
         contents = page.get_contents()
-        for index, content in enumerate(contents):
+        if (not isinstance(contents, pypdf.generic._data_structures.ArrayObject)):
+            # sometimes, there is only one content object
+            contents = [contents]
+        for content in contents:
             obj = content.get_object()
-            data = obj.get_data()
-            while (search in data):
-                data = data.replace(search, replace, 1)
-                page_replacements += 1
-            if (isinstance(obj, EncodedStreamObject)):
+            if (not isinstance(obj, pypdf.generic._data_structures.DecodedStreamObject)):
                 raise NotImplementedError("Modifying encoded (compressed) data streams is not supported. Uncompress input with qpdf's --qdf option should help.")
-                obj = DecodedStreamObject() # TODO: find out how to add this to output
-            obj.set_data(data)
-            contents[index] = obj
-
-        if (page_replacements > 0):
-            total_replacements += page_replacements
-            print(f"Replaced {page_replacements} occurrences on this page.")
+            content_stream = ContentStream(obj, reader, "bytes")
+            operations = content_stream.operations
+            operations = squash_TJs(operations)
+            operations = squash_TdTjs(operations, args.collapsible_width)
+            obj.set_data(operations_to_bytes(operations))
+            
         papersize = getattr(pypdf.PaperSize, args.papersize)
-        # TODO: find out how to preserve original mediabox
         page.mediabox = RectangleObject((0, 0, papersize.width, papersize.height))
         writer.add_page(page)
     if (args.output):
         writer.write(args.output)
-        print(f"Replaced {total_replacements} occurrences in document.")
