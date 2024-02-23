4ea30061862d3cc72add3bcacc8379eb6caa9e13
Hermann Höhne <hoehermann@gmx.de>
2024-02-23 12:01:08 +0100

    Can extract text from Inkscape, LibreOffice and Dmytro.

--- pypdf_strreplace.py
+++ pypdf_strreplace.py
@@ -6,7 +6,7 @@ import binascii
 import pypdf
 from typing import Any, Callable, Dict, Tuple, Union, cast
 from pypdf.generic import DictionaryObject, NameObject, RectangleObject, ContentStream
-from pypdf.generic._base import TextStringObject, FloatObject
+from pypdf.generic._base import TextStringObject, ByteStringObject, FloatObject
 from pypdf.constants import PageAttributes as PG
 from pypdf._cmap import build_char_map
 import pprint
@@ -17,13 +17,17 @@ class CharMap:
     @classmethod
     def from_char_map(cls, subtype:str, halfspace:float, encoding:Union[str, Dict[int, str]], map:Dict[str, str], ft:DictionaryObject):
         return cls(subtype, encoding, map, ft)
-    def decode(self, encoded):
-        if (isinstance(self.encoding, str) and self.encoding == "charmap"):
-            print("Decoding", encoded, "with this map:")
-            pprint.pprint(self.map)
-            return "".join([self.map[c] for c in encoded]) # TODO: use str.translate?
+    def decode(self, text:Union[TextStringObject,ByteStringObject]):
+        #print("Decoding", text.get_original_bytes(), "with this map:")
+        #pprint.pprint(self.map)
+        if (isinstance(text, TextStringObject) and self.encoding == "charmap"):
+            return "".join(text.get_original_bytes().decode('ascii').translate(str.maketrans(self.map)))
+        elif (isinstance(text, ByteStringObject)):
+            return "".join(text.decode(self.encoding).translate(str.maketrans(self.map)))
+        elif (isinstance(self.encoding, dict)):
+            return str(text)
         else:
-            raise NotImplementedError(f"Cannot decode with this encoding: {self.encoding}")
+            raise NotImplementedError(f"Cannot decode „{text}“ with this {type(self.encoding)} encoding: {self.encoding}")
 
 # from https://github.com/py-pdf/pypdf/blob/27d0e99/pypdf/_page.py#L1546
 def get_char_maps(obj: Any, space_width: float = 200.0):
@@ -46,11 +50,10 @@ def get_char_maps(obj: Any, space_width: float = 200.0):
     return cmaps
 
 class Context:
-    def __init__(self, charmaps, font:str = None):
-        self.charmaps = charmaps
+    def __init__(self, font:str = None):
         self.font = font
     def __copy__(self):
-        return type(self)(self.charmaps, self.font)
+        return type(self)(self.font)
 class PDFOperation:
     def __init__(self, operands, operator, context:Context):
         self.operands = operands
@@ -72,7 +75,7 @@ class PDFOperation:
             stream.write(b" ")
         stream.write(self.operator.encode("ascii"))
         stream.write(b"\n")
-    def get_text_map(self):
+    def get_text_map(self, charmaps):
         return []
 class PDFOperationTf(PDFOperation):
     def __init__(self, operands, context:Context):
@@ -85,29 +88,29 @@ class PDFOperationTJ(PDFOperation):
         super().__init__(operands, "TJ", context.__copy__())
     def __repr__(self):
         return f"„{self.operands[0]}“ {self.operator}"
-    def get_text_map(self):
+    def get_text_map(self, charmaps):
         map = []
-        for op in self.operands[0]:
-            if (isinstance(op, TextStringObject)):
-                map.append((op, self.context.charmaps[self.context.font].decode(op)))
+        for operand in self.operands[0]:
+            if (isinstance(operand, TextStringObject)):
+                map.append((operand, charmaps[self.context.font].decode(operand)))
         return map
 class PDFOperationTj(PDFOperation):
     def __init__(self, operands:list[TextStringObject], context:Context):
         if (len(operands) != 1):
-            raise ValueError(f"PDFOperationTJ expects one non-empty Array of TextStringObject")
+            raise ValueError(f"PDFOperationTj expects one non-empty Array of TextStringObject")
         super().__init__(operands, "Tj", context.__copy__())
     def __repr__(self):
         return f"„{self.operands[0]}“ {self.operator}"
-    def get_text_map(self):
-        return [(self.operands[0], self.context.charmaps[self.context.font].decode(self.operands[0]))]
+    def get_text_map(self, charmaps):
+        return [(self.operands[0], charmaps[self.context.font].decode(self.operands[0]))]
 
 def analyze_content(content:ContentStream, charmaps):
     #print(content.get_data())
     #pprint.pprint(content.operations)
-    context = Context(charmaps)
+    context = Context()
     operations = [PDFOperation.from_tuple(ops, op, context) for ops, op in content.operations]
     #pprint.pprint(operations)
-    text_maps = [op.get_text_map() for op in operations]
+    text_maps = [op.get_text_map(charmaps) for op in operations]
     text_maps = [tm for tm in text_maps if tm]
     for text_map in text_maps:
         print("".join([t[1] for t in text_map]))
