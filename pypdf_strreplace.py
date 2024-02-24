44b0c501ed8ce1bd6e461c303d2ad2e232909053
Hermann Höhne <hoehermann@gmx.de>
2024-02-24 16:12:49 +0100

    Searching and replacing works for the Inkscape sample.

--- pypdf_strreplace.py
+++ pypdf_strreplace.py
@@ -5,7 +5,7 @@ import io
 import binascii
 import pypdf
 from typing import Any, Callable, Dict, Tuple, Union, cast
-from pypdf.generic import DictionaryObject, NameObject, RectangleObject, ContentStream
+from pypdf.generic import DictionaryObject, NameObject, RectangleObject, ContentStream, ArrayObject
 from pypdf.generic._base import TextStringObject, ByteStringObject, NumberObject, FloatObject
 from pypdf.constants import PageAttributes as PG
 from pypdf._cmap import build_char_map
@@ -26,9 +26,20 @@ class CharMap:
         elif (isinstance(text, ByteStringObject)):
             return "".join(text.decode(self.encoding).translate(str.maketrans(self.map)))
         elif (isinstance(self.encoding, dict)):
-            return str(text)
+            return str(text) # TODO: find out if pypdf applies the encoding dict automatically or if all my samples use 1:1 mappings
         else:
             raise NotImplementedError(f"Cannot decode „{text}“ with this {type(self.encoding)} encoding: {self.encoding}")
+    def encode(self, text, reference):
+        #print("Encoding to conform to", type(reference))
+        #print(reference)
+        if (isinstance(self.encoding, dict)):
+            map = {v:k for k,v in self.encoding.items()}
+            return ByteStringObject(bytes([map[c] for c in text]))
+        elif (self.encoding == "charmap"):
+            map = {v:k for k,v in self.map.items()}
+            return ByteStringObject(text.translate(str.maketrans(map)).encode('ascii')) # encoding with ascii is a wild guess
+        else:
+            raise NotImplementedError(f"Cannot encode this {type(self.encoding)} encoding: {self.encoding}")
 
 # from https://github.com/py-pdf/pypdf/blob/27d0e99/pypdf/_page.py#L1546
 def get_char_maps(obj: Any, space_width: float = 200.0):
@@ -76,6 +87,10 @@ class PDFOperation:
         stream.write(b"\n")
     def get_text_map(self, charmaps):
         return [MappedOperand(self, None, None)]
+class PDFOperationTf(PDFOperation):
+    def __init__(self, operands, context:Context):
+        super().__init__(operands, "Tf", None)
+        context.font = operands[0]
 class PDFOperationTd(PDFOperation):
     def __init__(self, operands, context:Context):
         super().__init__(operands, "Td", None)
@@ -91,10 +106,6 @@ class PDFOperationTd(PDFOperation):
             # display horizontal adjustment as space. total guess. works for the xelatex sample.
             map.append(MappedOperand(self, None, " "))
         return map
-class PDFOperationTf(PDFOperation):
-    def __init__(self, operands, context:Context):
-        super().__init__(operands, "Tf", None)
-        context.font = operands[0]
 class PDFOperationTJ(PDFOperation):
     def __init__(self, operands:list[list[Union[TextStringObject,ByteStringObject,NumberObject]]], context:Context):
         if (len(operands) != 1):
@@ -112,6 +123,13 @@ class PDFOperationTJ(PDFOperation):
             else:
                 map.append(MappedOperand(self, operand, charmaps[self.context.font].decode(operand)))
         return map
+    def replace_text(self, text, charmaps, start, end):
+        start = self.operands[0].index(start)
+        end = self.operands[0].index(end)
+        pre = self.operands[0][:start]
+        post = self.operands[0][end+1:]
+        mid = [charmaps[self.context.font].encode(text, self.operands[0][start])]
+        self.operands[0] = ArrayObject(pre+mid+post)
 class PDFOperationTj(PDFOperation):
     def __init__(self, operands:list[Union[TextStringObject,ByteStringObject]], context:Context):
         if (len(operands) != 1):
@@ -121,13 +139,16 @@ class PDFOperationTj(PDFOperation):
         return f"„{self.operands[0]}“ {self.operator}"
     def get_text_map(self, charmaps):
         return [MappedOperand(self, self.operands[0], charmaps[self.context.font].decode(self.operands[0]))]
+    def replace_text(self, text, charmaps, start, end):
+        self.operands[0] = charmaps[self.context.font].encode(text, self.operands[0])
 
 def search_in_mappings(text_maps, needle):
     #print([[t.text for t in text_map if t.text] for text_map in text_maps])
+    # TODO: it would be better to have a list of (char → reference), search the needle, get the references by index. that would also allow the use of the regular expressions
     needle_index = 0
     start =  None
     end = None
-    for operation_level in text_maps:
+    for operation_level in text_maps: # TODO: remove this level
         for mapped_operand in operation_level:
             if (mapped_operand.text):
                 for haystack_index, c in enumerate(mapped_operand.text):
@@ -147,29 +168,58 @@ def search_in_mappings(text_maps, needle):
             break
     return (start, end)
 
-def search_text(content:ContentStream, charmaps, needle):
+def replace_operations(operations:list[PDFOperation], start:MappedOperand, end:MappedOperand, replacement, charmaps):
+    out = []
+    keep = True
+    for operation in operations:
+        if (keep):
+            out.append(operation)
+        if (operation == start.operation):
+            #print("found the start")
+            keep = False
+            replacement = start.text+replacement
+            if (operation == end.operation):
+                #print("start also is the end")
+                replacement += end.text
+                keep = True
+            operation.replace_text(replacement, charmaps, start.operand, end.operand)
+        if (operation == end.operation and start.operation != end.operation):
+            raise NotImplementedError("Replacing across multiple PDF text operations is not implemented.")
+            # print("found the end")
+            # keep = True
+            # operation.replace_text(replacement+end.text, charmaps)
+            # out.append(operation)
+    return out
+
+def replace_text(content:ContentStream, charmaps, needle, replacement):
     #print(content.get_data())
     #pprint.pprint(content.operations)
     context = Context()
     operations = [PDFOperation.from_tuple(ops, op, context) for ops, op in content.operations]
     #pprint.pprint(operations)
-    text_maps = [op.get_text_map(charmaps) for op in operations]
-    start, end = search_in_mappings(text_maps, needle)
-    print(start)
-    print(end)
+    while (True):
+        text_maps = [op.get_text_map(charmaps) for op in operations]
+        start, end = search_in_mappings(text_maps, needle)
+        #print(start)
+        #print(end)
+        if (start and end):
+            operations = replace_operations(operations, start, end, replacement, charmaps)
+        break # TODO: make it mork for more than one match per page
+    #pprint.pprint(operations)
     #text_maps = [tm for tm in text_maps if tm]
     #print("".join(["".join([t.text for t in text_map if t.text]) for text_map in text_maps]))
     #pprint.pprint(texts)
-    # stream = io.BytesIO()
-    # [op.write_to_stream(stream) for op in operations]
+    stream = io.BytesIO()
+    [op.write_to_stream(stream) for op in operations]
     # print(stream.getvalue())
-    #content.set_data(stream.getvalue())
+    content.set_data(stream.getvalue())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Replace text in a PDF file.')
     parser.add_argument('--input', type=str, required=True)
     parser.add_argument('--output', type=str)
     parser.add_argument('--search', type=str, required=True)
+    parser.add_argument('--replace', type=str, required=True)
     parser.add_argument('--papersize', type=str, default="A4")
     args = parser.parse_args()
     total_replacements = 0
@@ -185,9 +235,9 @@ if __name__ == "__main__":
         # NOTE: contents may be None, ContentStream, EncodedStreamObject, ArrayObject
         if (isinstance(contents, pypdf.generic._data_structures.ArrayObject)):
             for content in contents:
-                search_text(content, charmaps, args.search)
+                replace_text(content, charmaps, args.search, args.replace)
         elif (isinstance(contents, pypdf.generic._data_structures.ContentStream)):
-            search_text(contents, charmaps, args.search)
+            replace_text(contents, charmaps, args.search, args.replace)
         else:
             raise NotImplementedError(f"Cannot modify {type(contents)}.")
         page.replace_contents(contents)
