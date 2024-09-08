d1c29849e34b2933f72fafbf67af132f4dbc0a03
Hermann Höhne <hoehermann@gmx.de>
2024-09-08 19:42:04 +0200

    Added some comments.

--- pypdf_strreplace.py
+++ pypdf_strreplace.py
@@ -237,29 +237,30 @@ def schedule_changes(operations, matches, args_replace):
                             break
                     if (match):
                         if (len(text) >= match.end(0)):
+                            # we have enough text to cover the end of the current match
                             postfix = operand.plain_text[match.end(0)-previous_length:].strip("\n") # see prefix
                             postfix = match.re.sub(args_replace, postfix) if args_replace else postfix # see prefix
-                            print()
-                            print(f"{text[:match.start(0)]}»{text[match.start(0):match.end(0)]}«{text[match.end(0):]}".strip())
-                            print(f"prefix: „{prefix}“")
-                            print(f"infix: „{match.expand(args_replace) if args_replace else match.group(0)}“")
-                            print(f"postfix: „{postfix}“")
-                            print()
                             new_text = prefix+match.expand(args_replace)+postfix if args_replace else prefix+match.group(0)+postfix
                             first_operand.scheduled_change = Text(new_text)
                             if (operand != first_operand):
+                                # the match spans multiple operands
+                                # the first operand receives the replacement text in its entirety (with postfix)
+                                # we do not need the current operand anymore. mark current operand for deletion
                                 operand.scheduled_change = Delete()
+                            # we changed an operand in the operation the match begun in
+                            # this might be the current operation or a previous one
                             first_operation.scheduled_change = Change()
-                            print(f"{first_operation} must be changed.")
                             if (operation != first_operation):
-                                print(f"Current operation is not first operation and is marked to be deleted.")
-                                operation.scheduled_change = Delete() # Tj and Td cannot function with fewer operands
+                                # the match spans multiple operations
+                                # the first operations receives the replacement text in its entirety (with postfix)
+                                # we do not need the current operations anymore. mark current operations for deletion
+                                operation.scheduled_change = Delete()
                                 if (operation.operator in ["TJ"]):
                                     operand_changes = set([c.__class__.__name__ if c else c for c in [getattr(op, "scheduled_change", None) for op in operation.get_relevant_operands()]])
                                     if (operand_changes-set([Delete.__name__])):
-                                        print(f"But wait: Not all the operation's operands are going to be deleted! The operation must be changed, not deleted.")
+                                        #print(f"But wait: Not all the operation's operands are going to be deleted! The operation must be changed, not deleted.")
                                         operation.scheduled_change = Change()
-                            # match complete – reset
+                            # reset text gathering metadata for next match
                             match = None
                             first_operation = None
                             first_operand = None
@@ -267,50 +268,59 @@ def schedule_changes(operations, matches, args_replace):
                             # match exists, but the current text does not reach the end
                             # quit looking here and get more text
                             break
-            #print(f'{operand} scheduled_change is {getattr(operand, "scheduled_change", None)}')
             if (operation.operator in ["TJ", "Tj"] and first_operand is not None and not hasattr(operand, "scheduled_change")):
                 # delete operands containing replaced text
                 operand.scheduled_change = Delete()
         if (first_operation is not None and not hasattr(operation, "scheduled_change")):
+            # the match spans multiple operations
+            # the current operation (might be Tf or something else entirely) did not contain any text and must be removed to avoid confusion
             operation.scheduled_change = Delete()
         if (isinstance(getattr(operation, "scheduled_change", None), Delete) and operation.operator == "Td"):
+            # Td movement operations should not be deleted, but rather grouped together and moved behind the replacement
             operation.scheduled_change = Cluster()
 
 def replace_text(content, args_search, args_replace, gui_treeList):
     # transform plain operations to high-level objects
     operations = [PDFOperation.from_tuple(operands, operator, context) for operands, operator in content.operations]
+    
     # flatten mappings into one plain text string
     text = extract_text(operations)
     print("# These are the lines this tool might be able to handle:")
     print(text)
+
     matches = []
-    if (args_search):
     # search in text
+    if (args_search):
         matcher = re.compile(args_search)
         matches = list(matcher.finditer(text))
-        for match in matches:
-            print(match)
+
     # look up which operations contributed to each match
     schedule_changes(operations, matches, args_replace)
+    
+    # visualize content stream structure and scheduled changes
     if (gui_treeList):
         append_to_tree_list(operations, gui_treeList)
+
     if (args_replace):
         # do the replacements, but working backwards – else the indices would no longer match
+        # we iterate over the list of high-level operations, but we modify the pypdf low-level operations
         for operation_index, operation in reversed(list(enumerate(operations))):
             operation_change = getattr(operation, "scheduled_change", None)
             if (operation_change):
                 operation_change.apply(index=operation_index, collection=content.operations)
                 if (isinstance(operation_change, Delete)):
-                    print(f"Deleted: {operation}")
+                    #print(f"Deleted: {operation}")
+                    pass
                 elif (isinstance(operation_change, Cluster)):
-                    print(f"Moving together: {operation}")
+                    #print(f"Moving together: {operation}")
+                    pass
                 else:
-                    print(f"Before replacements: {operation}")
+                    #print(f"Before replacements: {operation}")
                     for operand_index, operand in reversed(list(enumerate(operation.get_relevant_operands()))):
                         operand_change = getattr(operand, "scheduled_change", None)
                         if (operand_change):
                             operand_change.apply(operation, operand_index, operation.get_relevant_operands())
-                    print(f"After replacements:  {operation}")
+                    #print(f"After replacements:  {operation}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Replace text in a PDF file.')
