d9c823e8b22d4c2fbe488b7beab770a1be9e4951
Hermann Höhne <hoehermann@gmx.de>
2026-04-24 23:30:58 +0200

    more glyphs with winansiencoding

--- createcontent.py
+++ createcontent.py
@@ -1,5 +1,5 @@
 import pypdf
-from pypdf.generic import NameObject, DictionaryObject, ContentStream, ArrayObject, NumberObject, TextStringObject
+from pypdf.generic import NameObject, DictionaryObject, ContentStream, ArrayObject, NumberObject, TextStringObject, ByteStringObject
 writer = pypdf.PdfWriter()
 page = pypdf.PageObject.create_blank_page(writer, 50, 20)
 page[NameObject('/Resources')] = DictionaryObject()
@@ -8,13 +8,14 @@ font_dict = DictionaryObject()
 font_dict[NameObject('/Type')] = NameObject('/Font')
 font_dict[NameObject('/Subtype')] = NameObject('/TrueType')
 font_dict[NameObject('/BaseFont')] = NameObject('/FreeSans')
+font_dict[NameObject('/Encoding')] = NameObject('/WinAnsiEncoding')
 page['/Resources']['/Font'][NameObject('/F1')] = font_dict
 content = ContentStream(None, page.pdf)
 content.operations = [
     (ArrayObject(), b'BT'),
     (ArrayObject([NumberObject(12), NumberObject(5)]), b'Td'),
     ([NameObject('/F1'), NumberObject(12)], b'Tf'),
-    (ArrayObject([TextStringObject('ABC')]), b'Tj'),
+    (ArrayObject([ByteStringObject('ÄB€'.encode("cp1252"))]), b'Tj'),
     (ArrayObject(), b'ET'),
     (ArrayObject(), b'Q'),
 ]
